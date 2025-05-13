be5da0024691c43e3459b80b3c3a8684795af7f3
MikelInesta <mikel_inesta@hotmail.com>
2025-05-13 16:15:16 +0200

    External language turtle

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,11 @@ func main(){
   instructions := make(map[string]turtle.Action)
   instructions["p"] = func (penNumber int){println("select pen ",penNumber)}
   instructions["d"] = func (_ int){println("pen down")}
+  instructions["n"] = func (distance int){println("draw north ",distance, "cm")}
+  instructions["s"] = func (distance int){println("draw south",distance, "cm")}
+  instructions["e"] = func (distance int){println("draw east",distance, "cm")}
+  instructions["w"] = func (distance int){println("draw west",distance, "cm")}
+  instructions["u"] = func (_ int){println("pen up")}
  
   t := turtle.NewTurtle(
     instructions,
